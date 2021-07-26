11d9192b498daa93f1e2b8b3230784f753bbb6b4
trungle2021 <leanhtrung97@gmail.com>
2021-07-26 18:02:31 +0700

    first commit

---
+++ haircare.js
@@ -0,0 +1,86 @@
+
+$(document).ready(function(){
+
+    $('.best-seller').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        nav: true,
+        navigation:true,
+        dots: false,
+        autoplay: true,
+        autoHeight:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:true
+            },
+            1000:{
+                items:3,
+                nav:true,
+                loop:true
+                
+            }
+        }
+    });
+
+    $('.ingredient-carousel').owlCarousel({
+        center: true,
+            items:3,
+            loop:true,
+            margin:30,
+            nav:false,
+            dots:false,
+            autoplay: true,
+            slideTransition: 'linear',
+            autoplayTimeout: 2000,
+            autoplaySpeed: 2000,
+            autoplayHoverPause: true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:5
+                }
+            }
+        })
+
+        $('.text1-carousel').owlCarousel({
+            center: true,
+                items:3,
+                loop:true,
+                margin:30,
+                nav:false,
+                dots:false,
+                autoplay: true,
+                slideTransition: 'linear',
+                autoplayTimeout: 5000,
+                autoplaySpeed: 5000,
+                autoplayHoverPause:false,
+                mouseDrag: false,
+                responsive:{
+                    0:{
+                        items:1
+                    },
+                    600:{
+                        items:1
+                    },
+                    1000:{
+                        items:2
+                    }
+                }
+            })
+//CLICK MOVE HAIR PROBLEMS PAGE 
+           $('#hairproblems-click').click(function(){
+            window.location.href = "http://127.0.0.1:5500/eproject/hair-problems.html";
+           })
+
+});
