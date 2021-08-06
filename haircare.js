124ab308aab3f7b202a7c7cf461e907fee10eb75
trungle2021 <leanhtrung97@gmail.com>
2021-08-06 21:08:23 +0700

    ok

--- haircare.js
+++ haircare.js
@@ -32,14 +32,14 @@ $(document).ready(function(){
         center: true,
             items:3,
             loop:true,
-            margin:30,
+            margin:60,
             nav:false,
             dots:false,
             autoplay: true,
             slideTransition: 'linear',
             autoplayTimeout: 2000,
             autoplaySpeed: 2000,
-            autoplayHoverPause: true,
+            autoplayHoverPause: false,
             responsive:{
                 0:{
                     items:1
@@ -48,7 +48,7 @@ $(document).ready(function(){
                     items:3
                 },
                 1000:{
-                    items:5
+                    items:6
                 }
             }
         })
@@ -113,6 +113,129 @@ $(document).ready(function(){
                 }
             });
         
-           
+            function showDialog2() {
+                $("#loginModal").removeClass("fade").modal("hide");
+                $("#registerModal").addClass("fade").modal("show");
+            }
+            function showLoginModal(){
+                $("#registerModal").removeClass("fade").modal("hide");
+                $("#loginModal").addClass("fade").modal("show");
+            }
+            
+            
+            $("#registerBtn").on("click", function() {
+                showDialog2();
+            });
+            
+            $("#alreadyacc").on("click",function(){
+                showLoginModal();
+            })
+
+            $("#formLogin").submit(function (e) {
+                e.preventDefault();
+      
+                Check();
+            })
+      
+      
+            $("#formRegister").submit(function (e) {
+                e.preventDefault();
+      
+                let username = $("#username1").val().trim();
+                let re = /^[a-z0-9_-]{3,16}$/ig;
+                if (re.test(username) == false) {
+                    alert("Invalid Username");
+                    $("#username1").focus();
+                    return false;
+                }
+      
+      
+                let email = $("#email1").val().trim();
+                re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ig;
+                if (re.test(email) == false) {
+                    alert("Invalid Email");
+                    $("#email1").focus();
+                    return false;
+                }
+      
+      
+                let password = $("#password1").val().trim();
+                re = /^[-\w\.\$@\*\!]{1,30}$/ig;
+                if (re.test(password) == false) {
+                    alert("Invalid Password! Password cannot contain white spaces!");
+                    $("#password1").focus();
+                    return false;
+                }
+      
+      
+                let passwordConfirm = $("#password2").val().trim();
+                if (passwordConfirm !== password) {
+                    alert("Password and confirm password not match! Please try again!")
+                    $("#password2").focus();
+                    return false;
+                }
+      
+                let gender = $("#gender:checked").val();
+      
+      
+      
+                let s = `Username: ${username} \n Email: ${email} \n Gender: ${gender}`;
+                alert("Register Success \n Your Information: \n " + s);
+                $("#registerModal").removeClass("fade").modal("hide");
+                store();
+      
+            })
+      
+      
+            function store() {
+                let username = $("#username1").val();
+                let email = $("#email1").val().trim();
+                let password = $("#password1").val();
+                let gender = $("#gender:checked").val();
+      
+                const user = {
+                    username: username,
+                    email: email,
+                    password: password,
+                    gender: gender,
+                }
+      
+                localStorage.setItem("user", JSON.stringify(user));
+                localStorage.getItem("user");
+                var myobj = JSON.parse(localStorage.getItem("user"));
+            }
+      
+            function Check() { //retrieves items in the localStorage
+      
+                var username = $("#username").val() //gets key from user
+                var password = $("#password").val(); //gets password from user
+                var myobj = JSON.parse(localStorage.getItem("user"));
+      
+                if (username == myobj.username && password == myobj.password) {
+                    alert("You are Logged in");
+                    $("#loginModal").removeClass("fade").modal("hide");
+                }
+                else {
+                    alert("Wrong username or password! Please try again!");
+                    $("#username").focus();
+                    return false;
+      
+                }
+            }
+        
+            $("#formContactFooter").submit(function (e) {
+                e.preventDefault();
+      
+                let email = $("#emailFooter").val().trim();
+                re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ig;
+                if (re.test(email) == false) {
+                    alert("Invalid Email");
+                    $("#email1").focus();
+                    return false;
+                }
+    
+                alert("Your message has been sent to us! Thank you for your contact!");
+      
+            })
         
 });
